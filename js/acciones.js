1c2fa6646efb1be8d05eaeef2fbe285d5f521544
karinarojas <karina3f.dsaupoo@gmail.com>
2016-05-25 14:15:25 -0500

    13subida

--- js/acciones.js
+++ js/acciones.js
@@ -10,7 +10,7 @@ $(document).ready(function(e) {
 		audio.preloadFX('do', 'audio/DO.mp3',function(msg){},function(msg){alert('Error: ' + msg ); }); 
 		audio.preloadFX('re', 'audio/RE.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
 		audio.preloadFX('mi', 'audio/MI.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
-		audio.preloadFX('fa', 'audio/FAmp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('fa', 'audio/FA.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
 		audio.preloadFX('sol', 'audio/SOL.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
 		audio.preloadFX('la', 'audio/LA.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
 		audio.preloadFX('si', 'audio/SI.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
